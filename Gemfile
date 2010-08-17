0a61c8482b4077856ad3ed38206f2ac73223046a
Alex Vollmer <alex.vollmer@gmail.com>
2010-08-17 13:02:59 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source :gemcutter
+
+gem "BlueCloth", "~>1.0"
+gem "rdiscount", "~>1.6"
+gem "haml", "~>3.0"
+gem "nanoc", "~>3.1"
+gem "highline", "~>1.5"
+gem "clip", "~>1.0"
+gem "nokogiri", "~>1.4"
+gem "compass", "~>0.10"
+gem "rack", "~>1.2"
---
+++ lib/nanoc_extensions.rb
@@ -0,0 +1,77 @@
+class Nanoc3::Site
+  def articles
+    items.select { |i| i.article? }.sort_by { |a| a.created_at }.reverse
+  end
+
+  def time
+    self.articles.sort_by { |a| a.created_at }.last.created_at
+  end
+
+  def article_count_for_year(year)
+    items.select { |i| i.article? && i.created_at.year == year }.size
+  end
+end
+
+class Nanoc3::Item
+  def name
+    identifier.split("/").last
+  end
+
+  def date_str
+    if self.created_at
+      d = self.created_at
+      sprintf("%4d/%02d", d.year, d.month)
+    end
+  end
+
+  def year
+    self.created_at.year
+  end
+
+  def month
+    self.created_at.month
+  end
+
+  def day
+    self.created_at.day
+  end
+
+  def display_date
+    self.created_at.strftime("%A, %d %B %Y")
+  end
+
+  def short_date
+    self.created_at.strftime("%d %B %Y")
+  end
+
+  def full_path
+    [self.date_str, sprintf("%02d", self.day), self.permalink.split("/").last].join("/")
+  end
+
+  def parent_path
+    self.full_path.split("/")[0..-3].join("/")
+  end
+
+  def extension
+    self[:extension] ||= (File.extname(self[:file].path) rescue nil)
+  end
+
+  def article?
+    "article" == attributes[:kind]
+  end
+
+  def tags
+    attributes[:tags] || []
+  end
+
+  def content
+    self.reps[0].content_at_snapshot(:pre)
+  end
+
+  # normally we'd implement respond_to?, but we want missing
+  # attributes to just fallback to whatever is in the attributes
+
+  def method_missing(msg, *args, &block)
+    attributes[msg.to_sym] || super
+  end
+end
